b3e7a784e8210df3123eb0889052218c04de7444
Jeremy Lee <jerlee@umass.edu>
2017-03-09 16:43:47 -0500

    export default added to userprofile.js

--- app/components/userprofile.js
+++ app/components/userprofile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class userprofile extends React.Component {
+export default class userprofile extends React.Component {
   render() {
     return (
       <p>User Profile</p>
