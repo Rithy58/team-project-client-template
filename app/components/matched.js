05605a946714948146552dce9453c3e4644bac6e
SeihakRithy Muth <Rithy58@gmail.com>
2017-03-09 16:42:50 -0500

    export matched.js

--- app/components/matched.js
+++ app/components/matched.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class matched extends React.Component {
+export default class matched extends React.Component {
   render() {
     return (
       <p>Matched</p>
