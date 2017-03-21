ddcf0caa8c3844d65638aae73f1e3b8bec35c424
Colin Stern <cstern19@gmail.com>
2017-03-21 16:53:49 -0400

    Created two React components for Search.

--- app/components/search.js
+++ app/components/search.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Search_Feed from './search_feed';
+import Search_Feed_Item from './search_feed_item';
 
-export default class search extends React.Component {
+export default class Search extends React.Component {
   render() {
     return (
-      <p>Search</p>
-    )
+          <div>
+            <Search_Feed
+              numberOfResults='14'
+              query='Algorithms'
+              sortType='Relevance'>
+              <Search_Feed_Item
+                pic='img/algorithm.jpg'
+                title='Introduction to Algorithms, 3rd Ed.'
+                author='Thomas H. Cormen'
+                location='Amherst, MA'></Search_Feed_Item>
+              <Search_Feed_Item
+                pic='img/algorithm1.jpg'
+                title='Algorithms, 4rd Ed.'
+                author='Robert Sedgewick'
+                location='Amherst, MA'></Search_Feed_Item>
+              <Search_Feed_Item
+                pic='img/algorithm.jpg'
+                title='Algorithms'
+                author='Sanjoy Dasgupta'
+                location='Amherst, MA'></Search_Feed_Item>
+            </Search_Feed>
+          </div>
+    );
   }
 }
-
-ReactDOM.render(<search />, document.getElementById('search'));
