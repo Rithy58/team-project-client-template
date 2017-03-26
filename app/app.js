6917a416f1e1e2a6a781a88889795df4c4f55cb8
Arta Razavi <artarazavi@gmail.com>
2017-03-26 18:46:03 -0400

    working on components

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
 
-//import home from './components/home.js';
+import Home from './components/home.js';
 import Userprofile from './components/userprofile.js';
 import Search from './components/search.js';
 import Matched from './components/matched.js';
@@ -20,11 +20,12 @@ if(document.getElementById('matched') !== null) {
   ReactDOM.render(
     <Userprofile />,
     document.getElementById('userprofile'));
+}else if(document.getElementById('home') !== null) {
+  ReactDOM.render(
+    <Home />,
+   document.getElementById('home'));
 }
-
 /*
-if(document.getElementById('home') !== null) {
-  ReactDOM.render(<home />, document.getElementById('home'));
 } else if(document.getElementById('search') !== null) {
   ReactDOM.render(<search />, document.getElementById('search'));
 } else if(document.getElementById('userprofile') !== null) {
--- app/components/home.js
+++ app/components/home.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+//import Home_Feed from './components/home_feed.js'
+import Home_Side_Bar from './home_side_bar.js';
 
-export default class home extends React.Component {
+export default class Home extends React.Component {
   render() {
     return (
-      <p>Home</p>
+      <div className="container">
+      <div className="home-feed">
+        <div className="words">
+            User Postings
+        </div>
+        //<Home_Feed/>
+        <Home_Side_Bar/>
+        document.getElementById('user-side-bar')
+      </div>
+    </div>
     )
   }
 }
 
-ReactDOM.render(<home />, document.getElementById('home'));
+//ReactDOM.render(<home />, document.getElementById('home'));
