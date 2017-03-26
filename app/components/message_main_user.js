a0fe290beaf508624b9457455f5e081dfb1d73ce
David Zagreda <dzagreda@umass.edu>
2017-03-26 19:18:17 -0400

    Finished message, message_main_user, message_sidebar_user

---
+++ app/components/message_main_user.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class Message_Main_User extends React.Component {
+  render() {
+    return (
+      <div className="media">
+        <span className="user1-img pull-left">
+          <img src={this.props.avatar } alt="avatar" className="img-circle" height="45"/>
+        </span>
+
+        <div className="media-body">
+          <a href="#">{this.props.author}</a>
+          <br />
+          {this.props.message}
+          <br />
+
+          <p className= "timestamp pull-right">
+            <span className="glyphicon glyphicon-time"></span>{this.props.timestamp}
+          </p>
+        </div>
+       </div>
+
+  )
+}
+}
