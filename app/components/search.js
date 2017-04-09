9336539c4995018e816e486c9d06c7b46206c816
Colin Stern <cstern19@gmail.com>
2017-04-09 16:21:22 -0400

    Updated search page to use Home_Feed_Item instead of Search_Feed_Item for consistency.

--- app/components/search.js
+++ app/components/search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Search_Feed from './search_feed';
-import Search_Feed_Item from './search_feed_item';
+import Home_Feed_Item from './home_feed_item';
 import Navbar from './navbar.js';
 
 export default class Search extends React.Component {
@@ -12,22 +12,28 @@ export default class Search extends React.Component {
               numberOfResults='14'
               query='Algorithms'
               sortType='Relevance'>
-              <Search_Feed_Item
-                pic='img/algorithm.jpg'
-                title='Introduction to Algorithms, 3rd Ed.'
+              <Home_Feed_Item
+                pic="https://upload.wikimedia.org/wikipedia/en/4/41/Clrs3.jpeg"
+                title='Introduction to Algorithms'
                 author='Thomas H. Cormen'
-                location='Amherst, MA'></Search_Feed_Item>
-              <Search_Feed_Item
+                edition="3rd Edition"
+                isbn='978-0262033848'
+                publisher='PubPub, Inc'></Home_Feed_Item>
+              <Home_Feed_Item
                 pic='img/algorithm1.jpg'
-                title='Algorithms, 4rd Ed.'
+                title='Algorithms'
                 author='Robert Sedgewick'
-                location='Amherst, MA'></Search_Feed_Item>
-              <Search_Feed_Item
-                pic='img/algorithm.jpg'
+                edition='4th Edition'
+                isbn='958-0492391326'
+                publisher='SpudHub, Inc.'></Home_Feed_Item>
+              <Home_Feed_Item
+                pic='img/algorithm2.jpg'
                 title='Algorithms'
                 author='Sanjoy Dasgupta'
-                location='Amherst, MA'></Search_Feed_Item>
-            </Search_Feed>
+                edition='6th Edition'
+                isbn='978-1113405806'
+                publisher='DubBub, Inc.'></Home_Feed_Item>
+                </Search_Feed>
           </div>
     );
   }
