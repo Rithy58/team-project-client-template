9f7246c9ba11422898ffb7d2a3c0e6f1c86c07b9
David Zagreda <dzagreda@umass.edu>
2017-04-10 12:48:01 -0400

    added functions to message to handle user input

--- app/components/message.js
+++ app/components/message.js
@@ -5,6 +5,32 @@ import Message_Main_User from './message_main_user.js'
 import Navbar from './navbar.js';
 
 export default class Message extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        value: ""
+      };
+    }
+
+    handleKeyUp(e) {
+      if (e.key === "Enter") {
+        // Prevent the event from "bubbling" up the DOM tree.
+        e.preventDefault();
+        var comment = this.state.value.trim();
+        if (comment !== "") {
+          // Post comment - send to Search
+          this.props.onSearch(this.state.value);
+          this.setState({value: ""});
+        }
+      }
+    }
+
+    handleChange(e) {
+      // Prevent the event from "bubbling" up the DOM tree.
+      e.preventDefault();
+      this.setState({value: e.target.value});
+    }
+
   render() {
     return (
     <div>
@@ -105,7 +131,8 @@ export default class Message extends React.Component {
 
 
                    <div className="input-group send-message">
-                     <input type="text" className="form-control" placeholder="Send a message..." />
+                     <input type="text" className="form-control" placeholder="Send a message..." value={this.state.value} onChange={(e) => this.handleChange(e)}
+                     onKeyUp={(e) => this.handleKeyUp(e)} />
                        <span className="input-group-btn">
                          <button type="submit" className="btn btn-default">
                            <span className="glyphicon glyphicon-envelope"></span>
