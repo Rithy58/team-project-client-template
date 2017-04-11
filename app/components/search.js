f8b3f9ee04404223e10fa534adf01e1211587808
Colin Stern <cstern19@gmail.com>
2017-04-11 16:43:17 -0400

    Modified Navbar to change form to div. Modified Search to properly set and read state.

--- app/components/search.js
+++ app/components/search.js
@@ -17,7 +17,8 @@ export default class Search extends React.Component {
     // Send to server.
     getQueryData(query, (queryData) => {
       // Database is now updated. Refresh the feed.
-      this.setState(queryData);
+
+      this.setState({results: [queryData]});
     });
   }
 
@@ -27,7 +28,7 @@ export default class Search extends React.Component {
           <div>
             <Navbar onSearch={(query) => this.onSearch(query)}/>
             <Search_Feed
-              numberOfResults='14'
+              numberOfResults='1'
               query='Algorithms'
               sortType='Relevance'>
               {this.state.results.map((feedItem) => {
@@ -42,27 +43,6 @@ export default class Search extends React.Component {
                     publisher={feedItem.publisher}></Home_Feed_Item>
                 )
               })}
-              <Home_Feed_Item
-                pic="https://upload.wikimedia.org/wikipedia/en/4/41/Clrs3.jpeg"
-                title='Introduction to Algorithms'
-                author='Thomas H. Cormen'
-                edition="3rd Edition"
-                isbn='978-0262033848'
-                publisher='PubPub, Inc'></Home_Feed_Item>
-              <Home_Feed_Item
-                pic='img/algorithm1.jpg'
-                title='Algorithms'
-                author='Robert Sedgewick'
-                edition='4th Edition'
-                isbn='958-0492391326'
-                publisher='SpudHub, Inc.'></Home_Feed_Item>
-              <Home_Feed_Item
-                pic='img/algorithm2.jpg'
-                title='Algorithms'
-                author='Sanjoy Dasgupta'
-                edition='6th Edition'
-                isbn='978-1113405806'
-                publisher='DubBub, Inc.'></Home_Feed_Item>
                 </Search_Feed>
           </div>
     );
