abc9e131c80d658186570325e382ea19676a9534
David Zagreda <dzagreda@umass.edu>
2017-04-25 16:58:06 -0400

    added database to message route

--- server/src/routes/message.js
+++ server/src/routes/message.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var messageRouter = express.Router();
+var db = require('../modules/db.js');
+var database = db.getDatabase();
 
 messageRouter.get('/', function(req, res) {
   res.status(200).send('fhhff');
