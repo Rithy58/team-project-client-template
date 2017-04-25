6bd891771cfb43d6b87bdcefbca33ac8d7b9f0f1
Colin Stern <cstern19@gmail.com>
2017-04-25 19:03:02 -0400

    Removed error handling and completed search.

--- server/src/routes/search.js
+++ server/src/routes/search.js
@@ -9,8 +9,6 @@ searchRouter.post('/', function(req, res) {
     var queryText = req.body.trim().toLowerCase();
     // Look for items within the database that contain queryText
     database.collection('items').find({
-      $or: feedData.contents.map((id) => { return {_id: id }}),
-      //TODO get rid of feedData above! does not exist - how can I get itemData?
       $text: {
         $search: queryText
       }
@@ -21,26 +19,10 @@ searchRouter.post('/', function(req, res) {
 
       // Resolve all items
       var resolvedItems = [];
-      var errored = false;
-      function onResolve(err, feedItem) {
-        if (errored) {
-          return;
-        } else if (err) {
-          errored = true;
-          sendDatabaseError(res,err);
-        } else {
-          resolvedItems.push(feedItem);
-          if (resolvedItems.length === items.length) {
-            // Send resolved items to the client
-            res.send(resolvedItems);
-          }
-        }
-      }
-
-      // Resolve all of the matched feed items in parallel.
       for (var i = 0; i < items.length; i++) {
-        getFeedItem(items[i]._id, onResolve);
+        resolvedItems.push(items[i]);
       }
+      res.send(resolvedItems);
 
       // Special case: No results.
       if (items.length === 0) {
