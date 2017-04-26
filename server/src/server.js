7cb6c51f2d7b8eacd8027e315b074294fbaf2f16
SeihakRithy Muth <Rithy58@gmail.com>
2017-04-25 21:43:20 -0400

    added urlencoded bodyparser

--- server/src/server.js
+++ server/src/server.js
@@ -17,6 +17,7 @@ var jwt = require('express-jwt')({
 // Middleware
 app.use(bodyParser.text());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('../client/build'));
 app.use(auth.initialize());
 
