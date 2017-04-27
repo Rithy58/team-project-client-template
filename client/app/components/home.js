91e603d7479ea5b204309a7bc01df9178ee244bb
SeihakRithy Muth <Rithy58@gmail.com>
2017-04-27 16:53:14 -0400

    added login

--- client/app/components/home.js
+++ client/app/components/home.js
@@ -7,9 +7,17 @@ export default class Home extends React.Component {
     return (
       <div className="row center-block">
         <div className="col-md-8">
-          <Home_Item title="Book 1" author="Author 1" isbn="1234" price="$120"></Home_Item>
-          <Home_Item title="Book 2" author="Author 2" isbn="5678" price="$140"></Home_Item>
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <h3 className="panel-title">Items Listing</h3>
+            </div>
+            <div className="panel-body">
+              <Home_Item title="Book 1" author="Author 1" isbn="1234" price="$120"></Home_Item>
+              <Home_Item title="Book 2" author="Author 2" isbn="5678" price="$140"></Home_Item>
+            </div>
+          </div>
         </div>
+
         <div className="col-md-4">
           <Home_Login></Home_Login>
         </div>
--- client/app/components/home/home_login.js
+++ client/app/components/home/home_login.js
@@ -3,13 +3,14 @@ import React from 'react';
 export default class Home_Login extends React.Component {
   render() {
     return (
-      <div className="panel panel-default">
-        <div className="panel-heading">
-          <h3 className="panel-title">Login</h3>
-        </div>
-        <div className="panel-body">
-          Panel content
-        </div>
+      <div>
+        <form className="form-signin">
+          <h2 className="form-signin-heading">Please login</h2>
+          <input type="text" className="form-control" placeholder="Username"/>
+          <input type="password" className="form-control" placeholder="Password"/>
+
+          <button className="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+        </form>
       </div>
     )
   }
