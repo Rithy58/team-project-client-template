6dd81482405f006ba7b4e4063956cbc9f7bbd772
SeihakRithy Muth <smuth@umass.edu>
2017-05-02 15:41:41 -0400

    addeding listing to db

---
+++ server/src/modules/listing.js
@@ -0,0 +1,29 @@
+var db = require('./db.js');
+var listing = {};
+
+listing.createListing = function(username, listing, cb) {
+  var database = db.getDatabase();
+  database.collection('listings').insertOne({
+    owner: username,
+    listing: listing
+  }, function(err, result) {
+    if(err) {
+      console.log(err);
+    }
+    console.log(result);
+    database.collection('users').updateOne({
+      _id: username
+    }, {
+      $push: {
+        listing: result._id
+      }
+    }, function(err, result) {
+      if(err) {
+        console.log(err);
+      }
+      cb(result);
+    });
+  });
+};
+
+module.exports = listing;
